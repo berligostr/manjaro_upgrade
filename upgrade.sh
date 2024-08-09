3688127102160ddc061c78be640fff6e21aec3ca
berligostr <57271192+berligostr@users.noreply.github.com>
2024-08-09 15:00:45 +0300

    пересборка Qt AUR

--- upgrade.sh
+++ upgrade.sh
@@ -102,6 +102,8 @@ if [[ "$updrep" = [yYlLдД] ]]; then
         if [[ "$parupd" = [yYlLдД] ]]; then echo -e "\n"; paru -Syyuu --repo | tee $HOME/upgrade.paru; fi
         # if [[ "$bekap" = [yYlLдД] ]]; then sudo sed -i 's/skipAutosnap=false/skipAutosnap=true/g' /etc/timeshift-autosnap.conf; fi
       fi
+    echo -e "\n"; read -n 1 -p "Пересобрать Qt пакеты из AUR? [y/N]: " uqta;
+    if [[ "$uqta" = [yYlLдД] ]]; then yay -S --rebuild $(pacman -Qmt | grep ^qt); fi
   fi
   #if [[ ! "$update" = [yYlLдД] ]]; then pamac upgrade --force-refresh --enable-downgrade --no-aur ; fi
   # echo -e "\n";
