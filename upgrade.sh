91f6ba63eb16cc9a45487ee5e1139e59f35b225e
berligostr <57271192+berligostr@users.noreply.github.com>
2024-10-23 18:13:22 +0300

    err4

--- upgrade.sh
+++ upgrade.sh
@@ -300,3 +300,4 @@ echo -e "\n";
 if [[ -f $HOME/upgrade.paru ]]; then rm $HOME/upgrade.paru; fi
 if [[ -f $HOME/upgrade.yay ]]; then rm $HOME/upgrade.yay; fi
 if [[ -f $HOME/upgrade.pamac ]]; then rm $HOME/upgrade.pamac; fi 
+
